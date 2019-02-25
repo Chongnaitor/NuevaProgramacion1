2d46e2c670414d917a5f5c0b699b3335cc0a4765
Chongnaitor <idv18c.jchong@uartesdigitales.edu.mx>
2019-02-25 15:42:18 -0600

    Se agrego la parte dos del drill

--- NuevoGit/LibroProgra/Libro/Capitulo 8/Use.cpp
+++ NuevoGit/LibroProgra/Libro/Capitulo 8/Use.cpp
@@ -9,12 +9,14 @@ int Drill()
 	int i = 99;
 	print(i);
 	std::cin.get();
+	return 0;
 }
 
 
 int swap(int a, int b)
 {
 	int temp; temp = a, a = b; b = temp;
+	return b, a;
 }
 		
 
@@ -23,7 +25,7 @@ int swap(int a, int b)
 int swap_r(int&a, int&b)
 {
 	int temp; temp = a, a = b; b = temp;
-
+	return a,b;
 }
 
 /*int swap_constant(const int&a, const int&b)
@@ -33,3 +35,12 @@ obviamente no va a compilar por que esta basado en constantes con referencias, o
 
 }
 */
+
+int main()
+{
+	int a = 7;
+	int b = 6;
+	swap(a, b);
+	std::cout<<b<<a;
+	std::cin.get();
+}
